a7244240cc8390833dc1e2ceac63ff74ff72df0e
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2015-12-01 14:14:18 +0100

    first commit

---
+++ ClarolineAngularUiSelectizeBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Claroline\AngularUiSelectizeBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Claroline\AngularUiSelectizeBundle\Installation\AdditionalInstaller;
+
+/**
+ * Bundle class.
+ * Uncomment if necessary.
+ */
+class ClarolineAngularUiSelectizeBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+}
