799ab7c6ae7b8378c9b56576a6a228b4f03294bb
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2015-12-09 17:10:06 +0100

    Fixing stuff.

--- ClarolineAngularUiSelectizeBundle.php
+++ ClarolineAngularUiSelectizeBundle.php
@@ -3,8 +3,18 @@
 namespace Claroline\AngularUiSelectizeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class ClarolineAngularUiSelectizeBundle extends Bundle
+class ClarolineAngularUiSelectizeBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environment)
+    {
+        return true;
+    }
 
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
